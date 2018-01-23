42bdb37195820ba1d33bd515106f6a7822c283d2
Liam Öberg <obergliam@gmail.com>
2018-01-23 12:16:44 +0100

    Form submit button test

--- src/__tests__/Posts.js
+++ src/__tests__/Posts.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, shallow, mount} from 'enzyme';
 import Posts from '../components/App';
+import CreateNewPost from '../components/CreateNewPost';
 
 test('renders the app', () => {
   render(<Posts />);
+});
+
+it('create post', () => {
+  const fakeFunction = jest.fn();
+  const form = shallow (<CreateNewPost author='hej' updatePosts={fakeFunction} />)
+  form.simulate('submit', {preventDefault () {}});
+  expect(fakeFunction).toHaveBeenCalledTimes(1);
 });
