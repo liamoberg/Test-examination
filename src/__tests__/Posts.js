703cdf5b057df07725a3b3822fb466b3d9877fba
Liam Öberg <obergliam@gmail.com>
2018-01-23 15:09:10 +0100

    Two tests that makes sure that the state is changed when you change values in post inputfields

--- src/__tests__/Posts.js
+++ src/__tests__/Posts.js
@@ -7,17 +7,25 @@ test('renders the app', () => {
   render(<Posts />);
 });
 
-it('Form submit button runs one function', () => {
+it('Form submit button runs a function', () => {
   const fakeFunction = jest.fn();
   const form = shallow (<CreateNewPost author='hej' updatePosts={fakeFunction} />)
   form.simulate('submit', {preventDefault () {}});
   expect(fakeFunction).toHaveBeenCalledTimes(1);
 });
 
-it.skip('Test if onChange sets value and name to state', () => {
+it('Test if onChange sets title to state', () => {
   const fakeFunction = jest.fn();
-  const form = shallow(<CreateNewPost author='hej' updatePosts={fakeFunction} value='' onChange={fakeFunction} />);
-  form.find('input').at(1).simulate('change');
-  expect(fakeFunction).toBeCalledWith('');
+  const form = mount(<CreateNewPost author='hej' updatePosts={fakeFunction} onChange={fakeFunction} />);
+  const title = {target : {name: 'title', value: 'asdasdasd'}};
+  form.find('input[name="title"]').simulate('change', title);
+  expect(form.state().title).toEqual('asdasdasd');
+})
 
-});
+it('Test if onChange sets content to state', () => {
+  const fakeFunction = jest.fn();
+  const form = mount(<CreateNewPost author='hej' updatePosts={fakeFunction} onChange={fakeFunction} />);
+  const content = {target : {name: 'content', value: 'sadasdasd'}};
+  form.find('textarea[name="content"]').simulate('change', content);
+  expect(form.state().content).toEqual('sadasdasd');
+})
