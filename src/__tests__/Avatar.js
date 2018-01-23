96f4b8a9883f9ce2155ec3eedbc25d696dd39d21
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-01-23 14:04:00 +0100

    100% avatarselector

--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import AvatarSelector from '../components/App';
 
 test('renders the avatarSelector', () => {
   render(<AvatarSelector />);
 });
 
-it('Avatarselector should be passed a string', () => {
+it('AvatarSelector should be passed a string', () => {
   const testImg = 'Testperson';
   expect(() => AvatarSelector(testImg)).toBeTruthy();
 });
 
-it('Avatarselector should throw error when not passed a string', () => {
+it('AvatarSelector should throw error when not passed a string', () => {
   const testImg = [];
   expect(() => AvatarSelector(testImg)).toThrow();
 });
 
+it('AvatarSelector should have 3 values', () => {
+  const wrapper = render(<AvatarSelector currentPersona='cpl' />);
+  const select = wrapper.find('option');
+  expect(select).toHaveLength(3);
+});
+
+it('AvatarSelector src should be same as currentPersona', () => {
+  const personas = ['Morgana', 'Esmeralda', 'Zac'];
+  const wrapper = mount(<AvatarSelector />);
+
+  wrapper.find('select').simulate('change', { target: { value: personas[0] } });
+  wrapper.find('select').simulate('change', { target: { value: personas[1] } });
+  wrapper.find('select').simulate('change', { target: { value: personas[2] } });
+
+  expect(
+    wrapper
+      .find('img')
+      .filterWhere(
+        item => item.prop('src') === personas[0] || personas[1] || personas[2]
+      )
+  ).toBeTruthy();
+});
