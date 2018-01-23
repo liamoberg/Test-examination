3d45be61c07a6402195d8849150fcf16bf602d42
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-01-23 14:31:05 +0100

    app tests

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../components/App';
 
-test('renders the app', () => {
-  render(<App />);
+it('if page is changed to home, currentPage should be bot ', () => {
+  const wrapper = shallow(<App />);
+  wrapper.instance().changePage();
+  wrapper.setState({ currentPage: 'bot' });
+  expect(wrapper.state().currentPage).toEqual('bot');
+});
+
+it('else it should just be home', () => {
+  const wrapper = shallow(<App />);
+  wrapper.setState({ currentPage: 'bot' });
+  wrapper.instance().changePage();
+  wrapper.setState({ currentPage: 'home' });
+  expect(wrapper.state().currentPage).toEqual('home');
 });
