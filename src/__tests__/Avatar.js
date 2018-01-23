3bd0977a33f31263d294159ca88f9a906547796c
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-01-23 10:46:01 +0100

    two tests for avatar

--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { render } from 'enzyme';
 import AvatarSelector from '../components/App';
 
-test('renders the app', () => {
+test('renders the avatarSelector', () => {
   render(<AvatarSelector />);
 });
+
+it('Avatarselector should be passed a string', () => {
+    const testImg = 'Testperson';
+    expect(() => AvatarSelector(testImg)).toBeTruthy();
+  });
+
+  it('Avatarselector should throw error when not passed a string', () => {
+    const testImg = [];
+    expect(() => AvatarSelector(testImg)).toThrow();
+  });
