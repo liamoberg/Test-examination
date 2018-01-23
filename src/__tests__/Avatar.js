9074c6e2c198f16151e9f60618d3076d589fb210
Gustav Sundström <sundstrom-gustav@hotmail.com>
2018-01-23 12:15:29 +0100

    more tests

--- src/__tests__/Avatar.js
+++ src/__tests__/Avatar.js
@@ -7,11 +7,12 @@ test('renders the avatarSelector', () => {
 });
 
 it('Avatarselector should be passed a string', () => {
-    const testImg = 'Testperson';
-    expect(() => AvatarSelector(testImg)).toBeTruthy();
-  });
+  const testImg = 'Testperson';
+  expect(() => AvatarSelector(testImg)).toBeTruthy();
+});
 
-  it('Avatarselector should throw error when not passed a string', () => {
-    const testImg = [];
-    expect(() => AvatarSelector(testImg)).toThrow();
-  });
+it('Avatarselector should throw error when not passed a string', () => {
+  const testImg = [];
+  expect(() => AvatarSelector(testImg)).toThrow();
+});
+
